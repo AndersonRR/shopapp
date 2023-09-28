14dbc5266acfa9c7299777a0f7e881381f5bb989
AndersonRR <anderson.ribeiro.r@outlook.com>
2023-09-28 13:45:53 -0300

    feat: adiciona tag para notificação

--- App.tsx
+++ App.tsx
@@ -9,12 +9,15 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
 OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 OneSignal.initialize('878f2e4d-8975-4e79-b767-d7dca8691a9d');
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('anderson@zenfisio.com');
 
   return (
     <NativeBaseProvider theme={THEME}>
